d50f8ed3b1ef2bcc3473e63a2d0f2c6e776a722b
ricardo-eth <ricardo.soaresdacosta@gmail.com>
2021-03-26 17:51:44 +0100

    Update

--- wcV2.js
+++ wcV2.js
@@ -1,11 +1,20 @@
 const fs = require('fs')
 
+if (process.argv.length === 3){
+  if (
+    process.argv[2] !== '-l' ||
+    process.argv[2] !== '-w' ||
+    process.argv[2] !== '-c' ){
+      console.log('usage: node wcV2.js -l or -w or -c file.txt')
+      process.exit(1)
+    } 
+}
+
 let text = fs.readFileSync(process.argv[3], 'utf-8')
 
 let line = text.split('\n').length
 let word = text.split(' ').length
 let caractere = text.length
-
 
 switch (process.argv[2]) {
   case '-l':
