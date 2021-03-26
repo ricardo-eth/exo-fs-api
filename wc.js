ec2e04518cad5da8bb1e9f97e75569292c9a9b85
ricardo-eth <ricardo.soaresdacosta@gmail.com>
2021-03-26 16:44:53 +0100

    Update.js

--- wc.js
+++ wc.js
@@ -1,5 +1,18 @@
 const fs = require('fs')
 
+
+
+if (!fs.existsSync(process.argv[2])) {
+  console.log(`Error: ${process.argv[2]} does not exist`)
+  process.exit(1)
+}
+
+const stats = fs.statSync(process.argv[2])
+if (!stats.isFile()) {
+  console.log(`Error: ${process.argv[2]} is not a file`)
+  process.exit(1)
+}
+
 let text = fs.readFileSync(process.argv[2], 'utf-8')
 
 let line = text.split('\n').length
