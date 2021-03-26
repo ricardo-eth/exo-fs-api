218ad8bc422c7b1cf76f06084d43725502cf9370
ricardo-eth <ricardo.soaresdacosta@gmail.com>
2021-03-26 18:17:46 +0100

    Update

--- wcV2.js
+++ wcV2.js
@@ -1,29 +1,32 @@
 const fs = require('fs')
 
-if (process.argv.length === 3){
-  if (
-    process.argv[2] !== '-l' ||
-    process.argv[2] !== '-w' ||
-    process.argv[2] !== '-c' ){
-      console.log('usage: node wcV2.js -l or -w or -c file.txt')
-      process.exit(1)
-    } 
-}
+let FILE = process.argv[process.argv.length -1]
 
-let text = fs.readFileSync(process.argv[3], 'utf-8')
+let text = fs.readFileSync(FILE, 'utf-8')
 
 let line = text.split('\n').length
 let word = text.split(' ').length
 let caractere = text.length
 
-switch (process.argv[2]) {
-  case '-l':
-    console.log(line + ' ' + process.argv[3])
-  break
-  case '-w':
-    console.log(word + ' ' + process.argv[3])
-  break
-  case '-c':
-    console.log(caractere + ' ' + process.argv[3])
-  break
+if (process.argv.length === 4){
+  switch (process.argv[2]) {
+    case '-l':
+      console.log(`${line} ${FILE}`)
+    break;
+    case '-w':
+      console.log(`${word} ${FILE}`)
+    break;
+    case '-c':
+      console.log(`${caractere} ${FILE}`)
+    break;
+    default:
+    console.log('usage: node wcV2.js -l or -w or -c file.txt')
+  }
+process.exit(1)
 }
+
+console.log(`${line} ${word} ${caractere} ${FILE}`)
+
+
+
+
